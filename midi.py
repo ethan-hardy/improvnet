5a49956b0dc3adcf19bb786c570b8bb99f681e07
Ethan Hardy <ehardy45@gmail.com>
2017-12-03 18:13:08 -0800

    midi processing

---
+++ midi.py
@@ -0,0 +1,45 @@
+from mido import MidiFile
+
+QUANTIZATION = 4 # 4 quantization steps per beat
+REST = -1
+
+def getNoteGroup(pitch, duration):
+    return [ { 'pitch': pitch, 'isNoteBeginning': ind == 0 } for ind in range(0, duration)]
+
+def createQuantizedList(track, ticksPerBeat):
+    # midi files put some space between note offs and subsequent
+    # note offs. we need to bridge these spaces so note lengths
+    # divide evenly and can be quantized for neural net input
+    for index, msg in enumerate(track):
+        if msg.type == 'note_on':
+            first_note_ind = index
+            break
+
+    track = track[first_note_ind:-1] # take out end of file message
+    notes = []
+
+    for msg in track:
+        # TODO: triplets
+        if msg.type == 'note_on':
+            restDuration = round(msg.time / ticksPerBeat * QUANTIZATION)
+            if restDuration > 0:
+                notes.extend(getNoteGroup(REST, restDuration))
+        elif msg.type == 'note_off':
+            noteDuration = round(msg.time / ticksPerBeat * QUANTIZATION)
+            notes.extend(getNoteGroup(msg.note, noteDuration))
+
+    return notes
+
+    # notes are in the form of
+    # { pitch: <int>, isNoteBeginning: <bool> }
+    # -1 pitch is for a rest
+
+
+def readFile():
+  file = MidiFile('ladybird.mid')
+
+  notes = createQuantizedList(file.tracks[0], file.ticks_per_beat)
+
+  import pdb; pdb.set_trace()
+
+readFile()
